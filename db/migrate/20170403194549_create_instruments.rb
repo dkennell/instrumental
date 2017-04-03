0f3260ba26f4bba332d7ed5d4b63ab89345960c7
David Kennell <davidkennell0@gmail.com>
2017-04-03 15:46:30 -0400

    generate instrument resource

---
+++ db/migrate/20170403194549_create_instruments.rb
@@ -0,0 +1,12 @@
+class CreateInstruments < ActiveRecord::Migration
+  def change
+    create_table :instruments do |t|
+      t.integer :rental_id
+      t.string :type
+      t.boolean :accessories
+      t.string :instrument_model
+
+      t.timestamps null: false
+    end
+  end
+end
